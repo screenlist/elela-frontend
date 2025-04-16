afca0c6570a53cb371e7d9290a29f951418a63c4
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-04-16 19:25:17 +0200

    The beginning

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Avatar } from "melt/builders";
+
+    const avatar = new Avatar({
+        src: "https://api.screenlist.co.za/storage/umjolo-the-gone-girl-jpeg1728754895410optmsd",
+    });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<img {...avatar.image} alt="Avatar" />
+<span {...avatar.fallback}>RH</span>
+
+<style>
+    [data-melt-avatar-image] {
+        width: 200px;
+    }
+</style>
