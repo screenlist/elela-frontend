655c404086e5fbc9c77c30d1a1012105b4006f75
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-04-24 22:29:20 +0200

    Set up layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import '../app.css';
+  import '@fontsource-variable/inter';
+  import '@fontsource-variable/jetbrains-mono';
+  import '@fontsource-variable/biorhyme';
+</script>
+<header class="pb-4 pt-6">
+  <nav class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+    </div>
+    <div class="navbar-center">
+      <a class="btn btn-circle text-xl" href="/">
+        <img src="/icon-dark.svg" alt="Elela icon" height="100px" width="100px" />
+      </a>
+    </div>
+    <div class="navbar-end">
+    </div>
+  </nav>
+</header>
+<main class="flex-grow pt-4">
+  <slot/>
+</main>
+<footer class="footer sm:footer-horizontal footer-center text-base-content p-4">
+  <div>
+    <span>Made with ❤️ in Braamfontein, Johannesburg</span>
+  </div>
+</footer>
