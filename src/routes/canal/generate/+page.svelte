19417717bbc258c425dcb91d6048556c23d58027
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-04-28 23:56:31 +0200

    Laid ground for a pricing page

---
+++ src/routes/canal/generate/+page.svelte
@@ -0,0 +1,75 @@
+<div class="flex flex-col justify-baseline items-center sm:flex-row sm:items-start sm:justify-center">
+  <div class="card mt-4 sm:m-4 max-w-sm w-1/2 min-w-xs bg-base-300 shadow-sm">
+    <div class="card-body">
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold">Free</h2>
+      </div>
+      <ul class="mt-6 flex flex-col gap-2 text-xs">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>High-resolution image generation</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>Customizable style templates</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>Batch processing capabilities</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>AI-driven image enhancements</span>
+        </li>
+        <li class="opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span class="line-through">Seamless cloud integration</span>
+        </li>
+        <li class="opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span class="line-through">Real-time collaboration tools</span>
+        </li>
+      </ul>
+      <div class="mt-6">
+        <button class="btn btn-primary btn-block">Subscribe</button>
+      </div>
+    </div>
+  </div>
+  <div class="card mt-4 sm:m-4 max-w-sm w-1/2 min-w-xs bg-base-300 shadow-sm">
+    <div class="card-body">
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold">Premium</h2>
+        <span class="text-xl">$1.39</span>
+      </div>
+      <ul class="mt-6 flex flex-col gap-2 text-xs">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>High-resolution image generation</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>Customizable style templates</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>Batch processing capabilities</span>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span>AI-driven image enhancements</span>
+        </li>
+        <li class="opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span class="line-through">Seamless cloud integration</span>
+        </li>
+        <li class="opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+          <span class="line-through">Real-time collaboration tools</span>
+        </li>
+      </ul>
+      <div class="mt-6">
+        <button class="btn btn-primary btn-block">Subscribe</button>
+      </div>
+    </div>
+  </div>
+</div>
