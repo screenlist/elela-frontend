4db116d8652cd8d355ac5b1c9b8f6b4a6bb57c69
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-08 12:17:47 +0200

    Added a simple error renderer

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/state';
+  import { TriangleAlert } from '@lucide/svelte'
+</script>
+
+<div class="flex flex-col justify-center items-center p-4">
+  <span class="alert alert-soft alert-error">
+    <TriangleAlert />
+    <h1 class="text-lg">{page.status}: {page.error.message}</h1>
+  </span>
+  
+</div>
