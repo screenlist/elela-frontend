d80255e2c252b09efd9775100567b6a7d58db6e3
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-08 12:19:24 +0200

    Added login and active session detection

---
+++ src/routes/+page.server.js
@@ -0,0 +1,62 @@
+import { error, fail, redirect } from '@sveltejs/kit'
+import { z }  from 'zod'
+import { PUBLIC_SERVER } from '$env/static/public'
+import { decodeJwt } from 'jose'
+
+export async function load({ cookies, url }){
+
+  const token = cookies.get('access_token')
+
+  if(token){
+    const claims = decodeJwt(token)
+    const valid = await fetch(`${PUBLIC_SERVER}/canal/session/poll?id=${claims.sid}`)
+    if(valid.ok){ 
+      return { logged_in: true }
+    } else {
+      return { logged_in: false }
+    }
+  } else {
+    return { logged_in: false }
+  }
+
+}
+
+export const actions = {
+  auth: async ({ request, cookies }) => {
+    const token = cookies.get('access_token')
+
+    if(token){
+      const claims = decodeJwt(token)
+      const valid = await fetch(`${PUBLIC_SERVER}/canal/session/poll?id=${claims.sid}`)
+      if(valid.ok){ redirect(303, '/canal') }
+    }
+
+    const data = await request.formData()
+    const passphrase = await data.get('passphrase')
+    const schema = z.string().trim().min(1, 'Enter a canal passphrase')
+    const validate = schema.safeParse(passphrase)
+    if(validate.success === false){
+      return fail(400, { autherror: JSON.parse(validate.error.message)[0]['message'] })
+    }
+    
+    const res = await fetch(`${PUBLIC_SERVER}/canal/auth`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({phrase: passphrase})
+      }
+    )
+    
+    if(!res.ok){ return fail(res.status, { autherror: await res.text()}) }
+
+    const canal = await res.json()
+
+    if(canal.access_token){
+      const claims = decodeJwt(canal.access_token)
+      cookies.set('access_token', canal.access_token, { sameSite: 'strict', path: '/', expires: new Date(claims.exp*1000) })
+      redirect(302, '/canal')
+    } else if(canal.auth_token){ 
+      redirect(302, '/canal/2fa')
+    }
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,68 @@
 <script>
-    import { House } from "@lucide/svelte";
+    import { House, Info, ArrowBigRightDash } from "@lucide/svelte"
+    import { enhance } from '$app/forms'
+
+    let { data, form } = $props()
+    console.log(data)
 </script>
 <div class="flex flex-col justify-items-start items-center p-4">
-    <div class="card card-border bg-base-300 max-w-sm w-full min-w-xs">
-        <div class="card-body">
-            <h1 class="card-title">Maritime waters await a sailor</h1>
-            <fieldset class="fieldset">
-                <label for="passphrase" class="label">Passphrase</label>
-                <input name="passphrase" type="password" class="input w-full" placeholder="all you need is six words" />
-            </fieldset>
-            <div class="card-actions flex-col">
-                <button class="btn btn-primary mt-4 w-full">Enter the waters</button>
-                <div class="divider">OR</div>
-                <a href="/canal/generate" class="btn btn-outline w-full">Generate a new canal</a>
-            </div>
+    {#if data?.logged_in === false}
+        <div class="card card-border card-sm bg-base-300 max-w-sm w-full min-w-xs">
+            <form class="card-body" method="POST" action="?/auth" use:enhance>
+                <h1 class="card-title uppercase">Canal</h1>
+                {#if form?.autherror}
+                    <div role="alert" class="alert alert-soft alert-error">
+                        <Info />
+                        <span>{form.autherror}</span>
+                    </div>
+                {/if}
+                <fieldset class="fieldset">
+                    <label for="passphrase" class="label">Passphrase</label>
+                    <input name="passphrase" type="password" class="input w-full" placeholder="all you need is six words" />
+                </fieldset>
+                <div class="card-actions flex-col">
+                    <button type="submit" class="btn btn-primary mt-4 w-full">Enter the waters</button>
+                    <div class="divider">OR</div>
+                    <a href="/canal/generate" class="btn btn-outline w-full">Generate a new canal</a>
+                </div>
+            </form>
         </div>
+    {:else if data?.logged_in === true}
+      <div class="card card-border card-sm bg-base-300 max-w-sm w-full min-w-xs">
+        <section class="card-body flex-row justify-between">
+          <h2 class="card-title uppercase">Canal</h2>
+          <div class="card-actions">
+            <a href="/canal" class="btn btn-primary w-full">
+              <ArrowBigRightDash />
+            </a>
+          </div>
+        </section>
+      </div>     
+    {/if} 
+    <div class="card card-border mt-4 card-sm bg-base-300 max-w-sm w-full min-w-xs">
+      <div class="card-body">
+        <h2 class="card-title uppercase">Flare</h2>
+        <div class="card-actions flex-row justify-evenly mt-4">
+          <a href="/wave" class="btn btn-neutral w-auto flex-1/2">Send a counterflare</a>
+          <a href="/bridge" class="btn btn-neutral w-auto flex-1/2">Approach the bridge</a>
+        </div>
+      </div>
     </div>
     <div class="card card-border mt-4 card-sm bg-base-300 max-w-sm w-full min-w-xs">
-        <div class="card-body">
-            <h2 class="card-title">Respond to a flare</h2>
-            <div class="card-actions flex-row justify-evenly mt-4">
-                <a href="/wave" class="btn btn-neutral w-auto flex-1/2">Send a counterflare</a>
-                <a href="/bridge" class="btn btn-neutral w-auto flex-1/2">Approach the bridge</a>
-            </div>
+      <div class="card-body">
+        <h2 class="card-title uppercase">Cargo</h2>
+        <fieldset class="fieldset">
+          <label for="passphrase" class="label">Driftkey</label>
+          <input name="phrase" type="password" class="input w-full" placeholder="💦 for your eyes only 😁👀" />
+          <div role="alert" class="alert alert-soft alert-info">
+            <Info />
+            <span>The driftkey is an emoji-words phrase used to access files made public</span>
+          </div>
+        </fieldset>
+        <div class="card-actions flex-row justify-end mt-4">
+          <a href="/cargo" class="btn btn-accent w-auto">Search</a>
         </div>
+      </div>
     </div>
 </div>
 
