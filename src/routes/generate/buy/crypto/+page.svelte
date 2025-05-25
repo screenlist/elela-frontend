ba987c57c557df5aa4829e10dfdb403fd26197e4
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-25 23:31:00 +0200

    Removed extra clean code

--- src/routes/generate/buy/crypto/+page.svelte
+++ src/routes/generate/buy/crypto/+page.svelte
@@ -74,12 +74,8 @@
 
     if(isInitialised) return
 
-    // clearStaleConnectionState()
-
     initialiseWallet()
     isInitialised = true
-
-    // checkAccountStatus()
 
     return () => { cleanUp() }
   })
@@ -90,6 +86,7 @@
   })
 
   function cleanUp(){
+    loading = false
     if(unsubscribe){
       unsubscribe()
       unsubscribe = null
@@ -98,52 +95,10 @@
       clearTimeout(paymentTimer)
       paymentTimer = null
     }
-    // if(modal){
-    //   try {
-    //     modal.disconnect('eip155')
-    //   } catch (error) {
-    //     console.log('Error during AppKit disconnect:', error)
-    //   }
-    // }
+    if(account){
+      modal.disconnect()
+    }
   }
-
-  // function clearStaleConnectionState(){
-  //   try {
-  //     const connectionStatus = localStorage.getItem('@appkit/connection_status')
-
-  //     let disconnectedFlag = false
-  //     for (let i = 0; i < localStorage.length; i++) {
-  //       const key = localStorage.key(i)
-  //       if (key && key.includes('wagmi') && key.includes('disconnected')) {
-  //         disconnectedFlag = localStorage.getItem(key) === 'true'
-  //         if (disconnectedFlag) break
-  //       }
-  //     }
-
-  //     if(
-  //       (connectionStatus === 'disconnected' && localStorage.getItem('@appkit/active_namespace')) || 
-  //       (disconnectedFlag && connectionStatus === 'connected')
-  //     ) {
-  //       console.log("Detected stale connection state, cleaning up...")
-  //       // for (let i = 0; i < localStorage.length; i++) {
-  //       //   const key = localStorage.key(i)
-  //       //   if (key && (
-  //       //       key.includes('@appkit') || 
-  //       //       key.includes('wagmi') || 
-  //       //       key.includes('wallet') ||
-  //       //       key.includes('connector') ||
-  //       //       key.includes('eip155')
-  //       //     )) {
-  //       //     try {
-  //       //       localStorage.removeItem(key)
-  //       //     } catch (e) {}
-  //       //   }
-  //       // }
-  //     }
-  //   } catch (error) {
-  //     console.log('Error clearing stale connection state:', error)
-  //   }
-  // }
 
   function initialiseWallet(){
     const metadata = {
@@ -181,17 +136,6 @@
 
 
   function handleAccountUpdate(acc){
-    // if(acc.isConnected){
-    //   try {
-    //     const connectionStatus = localStorage.getItem('@appkit/connection_status')
-    //     if(connectionStatus === 'disconnected'){
-    //       disconnect()
-    //       return
-    //     }
-    //   } catch (error) {
-    //     console.log('Error checking connection status:', error)
-    //   }
-    // }
     const isCorrectNetwork = typeof acc.caipAddress === 'string' ? acc.caipAddress.split(':').slice(0, 2).join(':') === `eip155:${activeNetwork.id}` : false
     if(acc.isConnected && isCorrectNetwork && acc.address !== 'undefined'){
       account = acc.address 
@@ -199,23 +143,6 @@
       account = null
     }
   }
-
-  // function checkAccountStatus(){
-  //   try {
-  //     const connectionStatus = localStorage.getItem('@appkit/connection_status')
-  //     if(connectionStatus === 'disconnected'){
-  //       disconnect()
-  //       return
-  //     }
-  //   } catch (error) {
-  //     console.log('Errorr checking connection status:', error)
-  //   }
-  //   try {
-  //     modal.getAccount('eip155')
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
 
   async function buy(){
     if(!account) return 
@@ -235,9 +162,7 @@
       })
 
       paymentTimer = setTimeout(() => {
-        loading = false
         goto(`/generate/phrase?ref=${data.crypto.reference}&sender=${account}`)
-        modal.disconnect()
       }, 1000*15)
       
     } catch (error) {
@@ -259,24 +184,6 @@
   function disconnect(){
     try {
       modal.disconnect('eip155')
-      
-      // for (let i = 0; i < localStorage.length; i++) {
-      //   const key = localStorage.key(i);
-      //   if (key && (
-      //       key.includes('@appkit') || 
-      //       key.includes('wagmi') || 
-      //       key.includes('wallet') ||
-      //       key.includes('connector') ||
-      //       key.includes('eip155')
-      //     )) {
-      //     try {
-      //       localStorage.removeItem(key)
-      //       console.log(`Cleared: ${key}`)
-      //     } catch (e) {
-      //       console.error(`Failed to remove ${key}:`, e)
-      //     }
-      //   }
-      // }
     } catch (error) {
       addToast({ message: error.message, type: 'error', auto: true })
       console.log('Disconnect error:', error)
@@ -285,7 +192,7 @@
 </script>
 
 <div class="flex flex-col justify-items-start items-center p-4">
-  <div class="card card-border bg-secondary text-base-100 max-w-lg w-full min-w-xs">
+  <div class="card card-border bg-accent max-w-lg w-full min-w-xs">
     <section class="card-body">
       <div class="avatar">
         <div class="w-24 rounded-full">
@@ -293,23 +200,29 @@
         </div>
       </div>
       <h1 class="card-title">Pay {data.crypto.price} {data.crypto.currency}</h1>
-      <div role="alert" class="alert alert-info">
-        <Info />
-        <span>You may incur a small gas fee.</span>
-      </div>
 
+      {#if loading}
+        <div role="alert" class="alert alert-soft alert-warning">
+          <div class="inline-grid *:[grid-area:1/1]">
+            <div class="status status-warning animate-ping"></div>
+            <div class="status status-warning"></div>
+          </div>
+          Processing payment, don't reload the page!
+        </div>
+      {/if}
+      
       <div class="card-actions flex-row">
         {#if typeof account === 'string'}
-          <button onclick={buy} class="btn btn-neutral flex-1">
+          <button disabled={loading} onclick={buy} class="btn btn-neutral flex-1">
             {#if loading}
               <span class="loading loading-spinner loading-sm"></span>
             {:else}
               Pay
             {/if}
           </button>
-          <button onclick={disconnect} class="btn btn-outline flex-1">Disconnect</button>
+          <button disabled={loading} onclick={disconnect} class="btn btn-outline flex-1">Disconnect</button>
         {:else}
-          <button onclick={connect} class="btn btn-neutral flex-1">Connect</button>
+          <button disabled={loading} onclick={connect} class="btn btn-neutral flex-1">Connect</button>
         {/if}
       </div>
     </section>
