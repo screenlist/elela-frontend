e12cc067d3a48d85bd1266cb2e2e204055acbf50
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-25 23:26:10 +0200

    Removed redundant  code

--- src/routes/canal/+layout.server.js
+++ src/routes/canal/+layout.server.js
@@ -2,28 +2,15 @@ import { error, fail, redirect } from '@sveltejs/kit'
 import { PUBLIC_SERVER } from '$env/static/public'
 
 export async function load({ cookies, url }){
+  const token = cookies.get('access_token')
+  const response = await fetch(`${PUBLIC_SERVER}/canal`, {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
 
-  const routes = [/.*\/generate\/?.*/]
-  const isPublic = routes.some(pattern => {
-		if(pattern instanceof RegExp){			
-			return pattern.test(url.pathname)
-		} else {
-			return pattern === url.pathname
-		}
-	})
-
-  if(!isPublic){
-    const token = cookies.get('access_token')
-    const response = await fetch(`${PUBLIC_SERVER}/canal`, {
-      method: 'GET',
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
-
-    if(!response.ok){
-      error(response.status, { message: await response.text() })
-    }
-
-    return await response.json()
+  if(!response.ok){
+    error(response.status, { message: await response.text() })
   }
-  
+
+  return {canal: await response.json()}
 }
--- src/routes/canal/+page.server.js
+++ src/routes/canal/+page.server.js
@@ -1,19 +1,16 @@
 import { error, fail, redirect } from '@sveltejs/kit'
-import { z }  from 'zod'
+import { PUBLIC_SERVER } from '$env/static/public'
 
-export async function load({ cookies }){
+export async function load({ cookies, url }){
   const token = cookies.get('access_token')
-}
+  const response = await fetch(`${PUBLIC_SERVER}/canal/session/validate`, {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
 
-export const actions = {
-  auth: async ({ request }) => {
-    const data = await request.formData()
-    const passphrase = await data.get('passphrase')
-    const schema = z.string().trim().min(1, 'Enter a canal passphrase')
-    const validate = schema.safeParse(passphrase)
-    if(validate.success === false){
-      return fail(400, { autherror: validate.error.message })
-    }
-    console.log(validate)
+  if(!response.ok){
+    error(response.status, { message: await response.text() })
   }
+
+  return {session: await response.json()}
 }
