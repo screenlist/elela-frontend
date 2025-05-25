164d6d1497aa5803f3e2cdc131d881c03ffb6b97
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-25 23:26:35 +0200

    Improved canal home page navigation

--- src/routes/canal/+layout.svelte
+++ src/routes/canal/+layout.svelte
@@ -5,22 +5,26 @@
 
   const path = $derived(page.url.pathname)
   const routesWithNoNav = ['/canal/generate', '/canal/generate/phrase']
-  const titles = {
-    '/canal': 'Home',
-    '/canal/bridges': 'Bridges',
-    '/canal/jetsam': 'Cargo',
-    '/canal/settings': 'Settings'
+
+  const getTitle = (path) => {
+    if (path.startsWith('/canal/bridges')) return 'Bridges'
+    if (path.startsWith('/canal/jetsam')) return 'Cargo'
+    if (path.startsWith('/canal/settings')) return 'Settings'
+    if (path === '/canal') return 'Home'
+    return ''
   }
+  const title = $derived(getTitle(path))
+  
 </script>
 
 {#if !routesWithNoNav.some(route => route === path)}
   <div class="flex justify-between items-center pl-4 sm:pl-12 sm:pr-8">
-    <h1 class="text-xl sm:text-2xl font-semibold">{titles[path]}</h1>
+    <h1 class="text-xl sm:text-2xl font-semibold">{title}</h1>
     <div role="tablist" class="tabs tabs-border tabs-lg">
       <a role="tab" href="/canal" class={`tab ${path === '/canal' ? 'tab-active' : ''}`}><Home  /></a>
-      <a role="tab" href="/canal/bridges" class={`tab ${path === '/canal/bridges' ? 'tab-active' : ''}`}><MessageCircle  /></a>
-      <a role="tab" href="/canal/jetsam" class={`tab ${path === '/canal/jetsam' ? 'tab-active' : ''}`}><FolderClosed  /></a>
-      <a role="tab" href="/canal/settings" class={`tab ${path === '/canal/settings' ? 'tab-active' : ''}`}><Settings  /></a>
+      <a role="tab" href="/canal/bridges" class={`tab ${/.*\/bridges\/?.*/.test(path) ? 'tab-active' : ''}`}><MessageCircle  /></a>
+      <a role="tab" href="/canal/jetsam" class={`tab ${/.*\/jetsam\/?.*/.test(path) ? 'tab-active' : ''}`}><FolderClosed  /></a>
+      <a role="tab" href="/canal/settings" class={`tab ${/.*\/settings\/?.*/.test(path) ? 'tab-active' : ''}`}><Settings  /></a>
     </div>
   </div>
 {/if}
