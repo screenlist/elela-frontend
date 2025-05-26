f3d63c6c1d9d208ee63eb5fd92334015ba507b43
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-26 22:16:10 +0200

    Fully implemented 2FA management

--- src/routes/canal/settings/access/+page.server.js
+++ src/routes/canal/settings/access/+page.server.js
@@ -3,7 +3,6 @@ import { PUBLIC_SERVER } from '$env/static/public'
 
 export const actions = {
   setup: async ({ request, cookies }) => {
-    console.log('it hits')
     const token = cookies.get('access_token')
     const res = await fetch(`${PUBLIC_SERVER}/canal/2fa/setup`,
       {
@@ -21,8 +20,47 @@ export const actions = {
   },
   enable: async ({ request, cookies }) => {
     const token = cookies.get('access_token')
+    const data = await request.formData()
+
+    const res = await fetch(`${PUBLIC_SERVER}/canal/2fa/enable`,
+      {
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json',
+          'User-Agent': request.headers.get('user-agent'), 
+          'Authorization': `Bearer ${token}` 
+        },
+        body: JSON.stringify({
+          auth_token: await data.get('auth_token'),
+          totp_token: await data.get('totp_token')
+        })
+      }
+    )
+    
+    if(!res.ok){ return fail(res.status, { autherror: await res.text()}) }
+
+    return { setup: await res.json() }
   },
   disable: async ({ request, cookies }) => {
     const token = cookies.get('access_token')
+    const data = await request.formData()
+
+    const res = await fetch(`${PUBLIC_SERVER}/canal/2fa/disable`,
+      {
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json',
+          'User-Agent': request.headers.get('user-agent'), 
+          'Authorization': `Bearer ${token}` 
+        },
+        body: JSON.stringify({
+          totp_token: await data.get('totp_token')
+        })
+      }
+    )
+    
+    if(!res.ok){ return fail(res.status, { autherror: await res.text()}) }
+
+    return { setup: await res.json() }
   }
 }
--- src/routes/canal/settings/access/+page.svelte
+++ src/routes/canal/settings/access/+page.svelte
@@ -29,13 +29,26 @@
   <div class="card-body">
     <h2 class="card-title">Secure Access</h2>
     <span class="opacity-50">Manage multi-factor authentication to secure your canal</span>
-    {#if data.canal.usage.topt_enabled && !setup}
-      <form method="POST" action="?/disable" use:enhance>
+    {#if data.canal.usage.totp_enabled && !setup}
+      <form method="POST" action="?/disable" use:enhance={({formData}) => {
+        if(token){ formData.append('auth_token', token) }
+        return async ({result, update}) => {
+          if(result.data?.autherror){ 
+            addToast({ message: result.data.autherror, type: 'error', auto: true }) 
+          }
+          await update()
+        }
+      }}>
+        <p class="alert alert-soft alert-success mb-4"><Info/>Your secure access is enabled, you need to use your authenticator app to enter the canal.</p>
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">Code</legend>
+          <input name="totp_token" type="text" class="input" placeholder="Enter code" />
+        </fieldset>
         <div class="card-actions">
           <button class="btn btn-neutral">Disable</button>
         </div>
       </form>
-    {:else if !data.canal.usage.topt_enabled && !setup}
+    {:else if !data.canal.usage.totp_enabled && !setup}
       <form method="POST" action="?/setup" use:enhance={() => {
         return async ({result, update}) => {
           console.log(result)
@@ -58,7 +71,7 @@
     {/if}
     {#if !data.canal.usage.topt_enabled && setup}
       <form method="POST" action="?/enable" use:enhance={({formData}) => {
-        if(authToken){ formData.append('auth_token', token) }
+        if(token){ formData.append('auth_token', token) }
         return async ({result, update}) => {
           if(result.data?.autherror){ 
             addToast({ message: result.data.autherror, type: 'error', auto: true }) 
@@ -78,7 +91,7 @@
         <span class="badge badge-neutral">{secret}</span>
         <fieldset class="fieldset">
           <legend class="fieldset-legend">Code</legend>
-          <input name="topt_token" type="text" class="input" placeholder="Type here" />
+          <input name="totp_token" type="text" class="input" placeholder="Enter code" />
         </fieldset>
         <div class="card-actions">
           <button class="btn btn-neutral">Enable</button>
