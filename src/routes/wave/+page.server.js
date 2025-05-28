77dd741c934cd0790f2ce64a79674f878450e179
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-05-28 23:17:57 +0200

    Implemented counter-flare creation

---
+++ src/routes/wave/+page.server.js
@@ -0,0 +1,31 @@
+import { error, fail, redirect } from '@sveltejs/kit'
+import { PUBLIC_SERVER } from '$env/static/public'
+
+export const actions = {
+  wave: async ({ request, cookies }) => {
+    const data = await request.formData()
+    const values = Object.fromEntries( Object.entries( Object.fromEntries(data.entries()) ).filter(([_, value]) => value != "") )
+
+    const flare = values.flare
+    const counterflare = values.counterflare
+    const anchor = values.passphrase
+
+    const res = await fetch(`${PUBLIC_SERVER}/canal/wave`,
+      {
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({
+          flare: flare,
+          counterflare: counterflare,
+          anchor: anchor
+        })
+      }
+    )
+    
+    if(!res.ok){ return fail(res.status, { posterror: await res.text()}) }
+
+    return { wave: await res.json() }
+  }
+}
--- src/routes/wave/+page.svelte
+++ src/routes/wave/+page.svelte
@@ -1,35 +1,113 @@
 <script>
-  import { Info } from '@lucide/svelte'
+  import { Calendar, Clock, Info } from '@lucide/svelte'
+  import { enhance } from '$app/forms'
+  import { addToast, cleanupToasts, dismissToast } from '$lib/toasts'
+  import { toasts } from '$lib/toasts.svelte.js'
+  import { onDestroy } from 'svelte'
+  import { addToCalendar } from '$lib/event'
+
+  let wave = $state(null)
+
+  function copyFlare(){
+    navigator.clipboard.writeText(wave.counterflare)
+    addToast({ message: 'Counter-flare successfully copied to clipboard', type: 'success', auto: true })
+  }
+
+  function saveEvent(){ addToCalendar({
+    title: `Meet with ${wave.flare}`,
+    start: new Date(wave.start_time),
+    end: new Date(wave.end_time),
+    location: 'Elela',
+    description: `Their flare was '${wave.flare}' and you replied with '${wave.counterflare}', visit app.elela.online/bridge to join the chat or check to if you have been accepted.`
+  }) }
+
+  const dateFormatter = new Intl.DateTimeFormat('en-ZA', { dateStyle: 'long' });
+  const timeFormatter = new Intl.DateTimeFormat('en-ZA', { timeStyle: 'short' });
+
+  onDestroy(() => {cleanupToasts()})
 </script>
 
 <div class="flex flex-col justify-items-start items-center p-4">
-  <div class="card card-border bg-base-300 max-w-lg w-full min-w-xs">
-    <div class="card-body">
-      <h1 class="card-title">Counterflare</h1>
-      <fieldset class="fieldset">
-          <label for="flare" class="fieldset-legent">Flare</label>
+  <div class="card card-border bg-base-300 max-w-sm w-full min-w-xs">
+    <form class="card-body" method="POST" action="?/wave" use:enhance={({formData}) => {
+      return async ({result, update}) => {
+        console.log(result)
+        if(result.data?.posterror){ 
+          addToast({ message: result.data.posterror, type: 'error', auto: true }) 
+        }
+        wave = result.data?.wave
+        await update()
+      }
+    }}>
+      {#if wave}
+        <h1 class="card-title">Share the Counter-Flare</h1>
+        <div role="alert" class="alert alert-soft alert-info">
+          <Info />
+          <span>Copy the counter-flare below as it is & share it with person who gave you the flare</span>
+        </div>
+        <span class="font-mono w-full p-4 bg-base-300 font-semibold text-center text-base border-dashed border-1 rounded-lg border-neutral">{wave.counterflare}</span>
+        <div class="card-actions flex-col">
+          <button type="button" onclick={copyFlare} class="btn btn-accent w-full">Copy</button>
+        </div>
+        <div class="divider"></div>
+        <div class="stats stats-vertical shadow">
+          <div class="stat place-items-center">
+            <div class="stat-figure text-neutral" >
+              <Calendar />
+            </div>
+            <div class="stat-title">Date</div>
+            <div class="stat-value">{dateFormatter.format(new Date(wave.start_time))}</div>
+          </div>
+          <div class="stat place-items-center">
+            <div class="stat-figure text-neutral" >
+              <Clock/>
+            </div>
+            <div class="stat-title">Time</div>
+            <div class="stat-value">{timeFormatter.format(new Date(wave.start_time))}</div>
+          </div>
+        </div>
+        <div class="card-actions">
+          <button type="button" onclick={saveEvent} class="btn btn-secondary flex-1">Download Calendar Event</button>
+        </div>
+        <div class="divider"></div>
+        <div class="card-actions flex-col">
+          <a href="/bridge" onclick={() => wave = null} class="btn btn-neutral w-full">Visit the Bridge</a>
+          <button type="button" onclick={() => wave = null} class="btn btn-outline w-full">Another Counter-Flare?</button>
+        </div>
+      {:else}
+        <h1 class="card-title">Counter-Flare</h1>
+        <fieldset class="fieldset">
+          <label for="flare" class="fieldset-legend">Flare</label>
           <span class="label">The initial phrase as exposed to you by a sailor</span>
-          <input name="flare" type="text" class="input w-full" placeholder="😎 worries aplenty 👨🏽‍🍳🥰" />
+          <input id="flare" name="flare" type="text" class="input w-full" placeholder="😎 worries aplenty 👨🏽‍🍳🥰" />
           <div class="divider"></div>
-          <label for="counterflare" class="fieldset-legent">Counterflare</label>
-          <span class="label">A two words reply phrase of at least 4 letters each</span>
-          <input name="counterflare" type="text" class="input w-full" placeholder="calmly breathe" />
+          <label for="counterflare" class="fieldset-legend">Counter-Flare</label>
+          <span class="label">A two word reply phrase of at least 4 letters each</span>
+          <input id="counterflare" name="counterflare" type="text" class="input w-full" placeholder="calmly breathe" />
           <div role="alert" class="alert alert-soft alert-info">
             <Info />
-            <span>The phrase will form a part of counterflare which you must show to the creator of the flare</span>
+            <span>The phrase will form a part of the counterflare which you must show to the creator of the flare</span>
           </div>
           <div class="divider"></div>
-          <label for="passphrase" class="fieldset-legent">Anchor</label>
+          <label for="passphrase" class="fieldset-legend">Anchor</label>
           <p class="label">A private passphrase you will need to join a bridge chat</p>
-          <input name="passphrase" type="password" class="input w-full" placeholder="Passphrase" />
+          <input id="passphrase" name="passphrase" type="password" class="input w-full" placeholder="Passphrase" />
           <div role="alert" class="alert alert-soft alert-info">
             <Info />
             <span>Never share the passphrase with anyone, treat it like your password and do not forget it</span>
           </div>
-      </fieldset>
-      <div class="card-actions flex-col">
-        <button class="btn btn-primary mt-4 w-full">Reply</button>
-      </div>
-    </div>
+        </fieldset>
+        <div class="card-actions flex-col">
+          <button type="submit" class="btn btn-primary mt-4 w-full">Reply</button>
+        </div>
+      {/if}
+    </form>
   </div>
+</div>
+<div class="toast toast-bottom toast-center">
+  {#each toasts as toast (toast.id) }
+    <div class={`alert alert-${toast.type}`}>
+      <span>{toast.message}</span>
+    </div>
+  {/each}
 </div>
