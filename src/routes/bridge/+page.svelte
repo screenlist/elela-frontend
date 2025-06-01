91ee40175c025e425179ec31ca3ad74cea81382e
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-06-01 09:42:44 +0200

    Better optimisation for approved and pending users

--- src/routes/bridge/+page.svelte
+++ src/routes/bridge/+page.svelte
@@ -3,9 +3,10 @@
   import { addToast, cleanupToasts, dismissToast } from '$lib/toasts'
   import { toasts } from '$lib/toasts.svelte.js'
   import { onDestroy, onMount } from 'svelte'
-  import { Info } from '@lucide/svelte'
+  import { Info, Calendar, Clock } from '@lucide/svelte'
   import { browser } from '$app/environment'
   import { getCookie } from '$lib/cookie'
+    import { invalidateAll } from '$app/navigation';
 
   let { data } = $props()
 
@@ -13,13 +14,14 @@
   let bridge = $state(null)
   let session = $state(null)
 
-  if(browser){ session = getCookie('wave_session') }
-
   const dateFormatter = new Intl.DateTimeFormat('en-ZA', { dateStyle: 'full' });
   const timeFormatter = new Intl.DateTimeFormat('en-ZA', { timeStyle: 'short' });
 
   onMount(() => {
-    if(data?.bridge){ bridge = data.bridge }
+    if(data?.bridge){ 
+      bridge = data.bridge 
+    }
+    session = getCookie('wave_session')
   })
 
   onDestroy(() => { cleanupToasts() })
@@ -27,36 +29,36 @@
 
 <div class="flex flex-col justify-items-start items-center p-4">
   <div class="card card-border bg-base-300 max-w-sm w-full min-w-xs">
-    {#if session}
+    {#if data?.bridge}
       <section class="card-body">
-        <h1 class="card-title">Response Status</h1>
+        <h1 class="card-title">Response status</h1>
         <div class="w-full text-center mt-2 mb-2">
-          <span class={`badge-xl  ${bridge.approved === true ? 'badge-success' : 'badge-warning'}`}>
-            ${bridge.approved === true ? 'Approved' : 'Pending'}
+          <span class={`badge-xl badge ${data.bridge.approved === true ? 'badge-success' : 'badge-warning'}`}>
+            {data.bridge.approved === true ? 'Approved' : 'Pending'}
           </span>
         </div>
-        {#if new Date(bridge.start_time) > new Date()}
+        {#if new Date(data.bridge.start_time) > new Date()}
           <div class="stats stats-vertical shadow">
             <div class="stat place-items-center">
               <div class="stat-figure text-neutral" >
                 <Calendar />
               </div>
               <div class="stat-title">Date</div>
-              <div class="stat-value">{dateFormatter.format(new Date(bridge.start_time))}</div>
+              <div class="stat-value">{dateFormatter.format(new Date(data.bridge.start_time))}</div>
             </div>
             <div class="stat place-items-center">
               <div class="stat-figure text-neutral" >
                 <Clock/>
               </div>
               <div class="stat-title">Time</div>
-              <div class="stat-value">{timeFormatter.format(new Date(bridge.start_time))}</div>
+              <div class="stat-value">{timeFormatter.format(new Date(data.bridge.start_time))}</div>
             </div>
           </div>
-        {:else if new Date(bridge.start_time) < new Date() && new Date(bridge.end_time > new Date())}
+        {:else if new Date(data.bridge.start_time) < new Date() && new Date(data.bridge.end_time > new Date())}
           <div class="stats stats-vertical shadow">
             <div class="stat place-items-center">
               <div class="stat-title">Ending in</div>
-              <div class="stat-value"><span>{Math.ceil( (new Date(bridge.end_time).valueOf() - Date.now()) / (1000*60) )}</span></div>
+              <div class="stat-value"><span>{Math.ceil( (new Date(data.bridge.end_time).valueOf() - Date.now()) / (1000*60) )}</span></div>
               <div class="stat-desc">Minutes</div>
             </div>
           </div>
@@ -64,12 +66,12 @@
           <div class="stats stats-vertical shadow">
             <div class="stat place-items-center">
               <div class="stat-title">Ended</div>
-              <div class="stat-value"><span>{Math.ceil( ( Date.now() - new Date(bridge.end_time).valueOf() ) / (1000*60) )}</span></div>
+              <div class="stat-value"><span>{Math.ceil( ( Date.now() - new Date(data.bridge.end_time).valueOf() ) / (1000*60) )}</span></div>
               <div class="stat-desc">Minutes ago</div>
             </div>
           </div>
         {/if}
-        {#if bridge.approved}
+        {#if data.bridge.approved}
           <form method="POST" action="?/logout" class="card-actions" use:enhance={({formData}) => {
             return async ({result, update}) => {
               if(result.data?.posterror){ 
@@ -83,18 +85,18 @@
             }
           }}>
             <button type="submit" class="btn btn-outline">Logout</button>
-            {#if bridge.connection_path}
-              <a href={bridge.connection_path} class="btn btn-primary flex-1" >Join</a>
+            {#if data.bridge.connection_id}
+              <a href={`/bridge/${data.bridge.connection_id}`} class="btn btn-primary flex-1" >Join</a>
             {/if}
           </form>
         {/if}
       </section>
     {:else if queried}
       <section class="card-body">
-        <h1 class="card-title">Response Status</h1>
+        <h1 class="card-title">Response status</h1>
         <div class="w-full text-center mt-2 mb-2">
-          <span class={`badge-xl  ${bridge.approved === true ? 'badge-success' : 'badge-warning'}`}>
-            ${bridge.approved === true ? 'Approved' : 'Pending'}
+          <span class={`badge badge-xl  ${bridge.approved === true ? 'badge-success' : 'badge-warning'}`}>
+            {bridge.approved === true ? 'Approved' : 'Pending'}
           </span>
         </div>
         {#if new Date(bridge.start_time) > new Date()}
@@ -145,8 +147,8 @@
             }
           }}>
             <button type="submit" class="btn btn-outline">Logout</button>
-            {#if bridge.connection_path}
-              <a href={bridge.connection_path} class="btn btn-primary flex-1" >Join</a>
+            {#if bridge.connection_id}
+              <a href={`/bridge/${data.bridge.connection_id}`} class="btn btn-primary flex-1" >Join</a>
             {/if}
           </form>
         {/if}
@@ -154,6 +156,7 @@
     {:else}
       <form method="POST" action="?/auth" class="card-body" use:enhance={({formData}) => {
         return async ({result, update}) => {
+          console.log(result)
           if(result.data?.posterror){ 
             addToast({ message: result.data.posterror, type: 'error', auto: true }) 
           }
