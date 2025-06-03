71c5709184073a1438d7c8ab9e593f3317fe8266
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-06-03 09:10:52 +0200

    Added cookie subdomain functionality

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
 import { error, fail, redirect } from '@sveltejs/kit'
 import { z }  from 'zod'
-import { PUBLIC_SERVER } from '$env/static/public'
+import { PUBLIC_SERVER, PUBLIC_APP_ENV, PUBLIC_COOKIE_DOMAIN } from '$env/static/public'
 import { decodeJwt } from 'jose'
 
 export async function load({ cookies, url, request }){
@@ -18,8 +18,10 @@ export async function load({ cookies, url, request }){
 
 export const actions = {
   auth: async ({ request, cookies }) => {
-    const token = cookies.get('access_token')
+    const visitor = cookies.get('visitor_token')
+    if(visitor){ return fail(400, { autherror: 'Please log out of your bridge first'}) }
 
+    const token = cookies.get('access_token')
     if(token){
       const claims = decodeJwt(token)
       const valid = await fetch(`${PUBLIC_SERVER}/canal/session/poll?id=${claims.sid}`)
@@ -48,7 +50,13 @@ export const actions = {
 
     if(canal.access_token){
       const claims = decodeJwt(canal.access_token)
-      cookies.set('access_token', canal.access_token, { sameSite: 'strict', path: '/', expires: new Date(claims.exp*1000) })
+      const cookieOptions = { 
+        sameSite: 'strict', 
+        path: '/', 
+        expires: new Date(claims.exp*1000)
+      }
+      if(PUBLIC_APP_ENV === 'production'){ cookieOptions.domain = PUBLIC_COOKIE_DOMAIN }
+      cookies.set('access_token', canal.access_token, cookieOptions)
       cookies.set('canal_session', claims.sid, { sameSite: 'strict', path: '/', expires: new Date(claims.exp*1000), httpOnly: false })
       redirect(302, '/canal')
     } else if(canal.auth_token){ 
--- src/routes/bridge/+page.server.js
+++ src/routes/bridge/+page.server.js
@@ -1,5 +1,5 @@
 import { error, fail, redirect } from '@sveltejs/kit'
-import { PUBLIC_SERVER } from '$env/static/public'
+import { PUBLIC_SERVER, PUBLIC_APP_ENV, PUBLIC_COOKIE_DOMAIN } from '$env/static/public'
 import { decodeJwt } from 'jose'
 
 export async function load({ cookies, url }){
@@ -19,6 +19,10 @@ export async function load({ cookies, url }){
 
 export const actions = {
   auth: async ({ request, cookies }) => {
+    const token = cookies.get('access_token')
+
+    if(token){ return fail(400, { posterror: 'Please log out of your canal first'}) }
+
     const data = await request.formData()
     const values = Object.fromEntries( Object.entries( Object.fromEntries(data.entries()) ).filter(([_, value]) => value != "") )
 
@@ -46,7 +50,13 @@ export const actions = {
 
     if(auth.approved === true){
       const claims = decodeJwt(auth.access_token)
-      cookies.set('visitor_token', auth.access_token, { sameSite: 'strict', path: '/', expires: new Date(claims.exp*1000) })
+      const cookieOptions = { 
+        sameSite: 'strict', 
+        path: '/', 
+        expires: new Date(claims.exp*1000)
+      }
+      if(PUBLIC_APP_ENV === 'production'){ cookieOptions.domain = PUBLIC_COOKIE_DOMAIN }
+      cookies.set('visitor_token', auth.access_token, cookieOptions)
       cookies.set('wave_session', claims.sid, { sameSite: 'strict', path: '/', expires: new Date(claims.exp*1000), httpOnly: false })
       auth.access_token = null
       return { bridge: auth }
