450d223754cf78121b576ae91e1027c277bb6588
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-06-14 11:33:17 +0200

    Added a picture to the error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,10 +3,12 @@
   import { TriangleAlert } from '@lucide/svelte'
 </script>
 
-<div class="flex flex-col justify-center items-center p-4">
-  <span class="alert alert-soft alert-error">
+<div class="flex flex-col justify-center w-full h-full items-center p-4">
+  <figure class="w-full max-w-sm">
+    <img src="/deadend.svg" alt="An illustration with the cancel icon">
+  </figure>
+  <span class="alert alert-soft alert-error mt-4">
     <TriangleAlert />
     <h1 class="text-lg">{page.status}: {page.error.message}</h1>
   </span>
-  
 </div>
